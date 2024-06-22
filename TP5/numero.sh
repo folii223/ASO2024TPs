ff52bb30dd60001b9254a01d8b817e6f495d4597
folii223 <hernan.folik@gmail.com>
2024-06-22 17:00:59 -0300

    TP5

--- TP5/numero.sh
+++ TP5/numero.sh
@@ -7,9 +7,9 @@ while test $numero_aleatorio -ne $numero_ing;
 	do
 	   read -p "Vas a tener que  adivinar un número del 1 al 100. Ingresa un número: " numero_ing
 		if test $numero_aleatorio -gt $numero_ing;
-			then  echo "El número a adivinar es más grande"
+			then  echo "El número a adivinar es mayor"
 		elif test $numero_ing -gt $numero_aleatorio;
-			then echo "El número a adivinar es más chico"
-		else echo "Felicitaciones. Has adivina el número. Era el $numero_aleatorio."
+			then echo "El número a adivinar es menor"
+		else echo "Felicitaciones. Adivinaste el número. Era: $numero_aleatorio."
 		fi
 done
